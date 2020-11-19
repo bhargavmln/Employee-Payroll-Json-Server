e0720d7a7ef2dc5b65a6eb4d541585c3693214aa
bhargavmln <bhargavmln@gmail.com>
2020-11-19 17:47:10 +0530

    [bhargavmln]add:ability to add event listeners

--- js/EmployeeForm.js
+++ js/EmployeeForm.js
@@ -1,16 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
 
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
-});
-
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function () {
-    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if (nameRegex.test(text.value))
-        textError.textContent = "";
-    else textError.textContent = "Name is Incorrect";
-});
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+})
