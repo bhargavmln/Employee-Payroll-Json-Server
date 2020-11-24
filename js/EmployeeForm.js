8fa9ac27bd049e3f210430ed4a9492af701e9a7b
bhargavmln <bhargavmln@gmail.com>
2020-11-24 21:51:33 +0530

    [bhargavmln]add:ability to edit payroll form by clicking on edit icon

--- js/EmployeeForm.js
+++ js/EmployeeForm.js
@@ -1,3 +1,6 @@
+let isUpdate = false;
+let employeePayrollObj = {};
+
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
@@ -19,6 +22,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     salary.addEventListener('input', function () {
         output.textContent = salary.value;
     });
+
+    checkForUpdate();
 
     const year = document.querySelector('#year');
     const dateError = document.querySelector('.date-error');
@@ -126,4 +131,37 @@ const getInputValueById = (id) => {
     return value;
 }
 
+const checkForUpdate = () => {
+    const employeePayrollJson = localStorage.getItem('editEmp');
+    isUpdate = employeePayrollJson ? true : false;
+    if(!isUpdate) return;
+    employeePayrollObj = JSON.parse(employeePayrollJson);
+    setForm();
+}
 
+const setSelectedValue = (propertyValue , value) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        if(Array.isArray(value)) {
+            if(value.includes(item.value)){
+                item.checked = true;
+            }
+        }
+        else if(item.value === value)
+        item.checked=true;
+    })
+}
+
+const setForm = () => {
+    setValue('#name', employeePayrollObj._name);
+    setSelectedValue('[name=profile]',employeePayrollObj._profilePic);
+    setSelectedValue('[name=gender]', employeePayrollObj._gender);
+    setSelectedValue('[name=department]' , employeePayrollObj._department);
+    setValue('#salary', employeePayrollObj._salary);
+    setTextValue('.salary-output' , employeePayrollObj._salary);
+    setValue('#notes', employeePayrollObj._note);
+    let date = stringifyDate(employeePayrollObj._startDate).split(" ");
+    setValue('#day', date[0]);
+    setValue('#month',date[1]);
+    setValue('#year', date[2]);
+}
